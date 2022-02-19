f078a389e694597c0ccbb1b1698cba7712007dda
Aviel Hernandez <70616151+dez03@users.noreply.github.com>
2022-02-19 01:57:41 -0500

    Sign up, resources, FAQ completed.

---
+++ zombie apocalypse club 2/signup.js
@@ -0,0 +1,34 @@
+function emailValidator() {
+  let form = document.getElementById("form");
+  let email = document.getElementById("email").value;
+  let text = document.getElementById("text");
+  let thanksbutn = document.getElementById("thanksbutn");
+  let mailformat =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  if (email.match(mailformat)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "Your email is valid";
+    text.style.color = "#00ff00";
+    thanksbutn.href = "thankyou.html";
+  } 
+  else {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML = "Please enter valid email";
+    text.style.color = "#ffffff";
+  }
+  if (email == "") {
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+  }
+}
+
+function signupbtn() {
+  if (emailValidator == true) {
+    alert("good");
+  }
+}
+
